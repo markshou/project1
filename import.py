7533f8d831a6668d7b7dfb4a39e72d3980b33fd1
markshou <45491123+markshou@users.noreply.github.com>
2018-12-20 22:42:29 -0500

    Update import.py

--- import.py
+++ import.py
@@ -4,7 +4,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-# engine = create_engine("postgres://oydqwwvmfkwawe:3cb6a5a0d105c929fc4695244b25dfbb1458befabddb2b3c85d254549213b656@ec2-107-21-125-209.compute-1.amazonaws.com:5432/dcmoh597l9rarv")
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
