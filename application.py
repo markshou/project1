423f3ec2d6e6895220e168cf17728300946c1324
markshou <45491123+markshou@users.noreply.github.com>
2018-12-20 22:42:01 -0500

    Update application.py

--- application.py
+++ application.py
@@ -24,7 +24,6 @@ Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
-""" engine = create_engine("postgres://oydqwwvmfkwawe:3cb6a5a0d105c929fc4695244b25dfbb1458befabddb2b3c85d254549213b656@ec2-107-21-125-209.compute-1.amazonaws.com:5432/dcmoh597l9rarv") """
 db = scoped_session(sessionmaker(bind=engine))
 
 
